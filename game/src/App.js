663771a4c0004bb50dbd4e40a7d36c27c2cdc195
Daniel <danieljbloch95@gmail.com>
2020-02-06 16:18:43 -0500

    Started Adding CSS

--- game/src/App.js
+++ game/src/App.js
@@ -1,21 +1,41 @@
 import React from 'react'
-import Question from './Components/Question.js'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       baseURL: 'http://jservice.io/api/random',
-      score: '0',
       category: '',
       points: '',
-      answer: ''
+      answer: '',
+      question: '',
+      score: '0',
+      show: false
     }
     this.increasePoints = this.increasePoints.bind(this)
     this.decreasePoints = this.decreasePoints.bind(this)
     this.toggleAnswer = this.toggleAnswer.bind(this)
     this.revealQuestion = this.revealQuestion.bind(this)
   }
+
+  increasePoints(){
+    this.setState({
+      score: this.state.score + this.state.points
+    })
+  }
+
+  decreasePoints(){
+    this.setState({
+      score: this.state.score - this.state.points
+    })
+  }
+
+  toggleAnswer(){
+    this.setState({
+      show: !this.state.answer
+    })
+  }
+
   revealQuestion(){
     fetch('http://jservice.io/api/random')
     .then(response => {
@@ -23,32 +43,11 @@ class App extends React.Component {
     }).then(json => this.setState({
       Question: json[0].question,
       Answer: json[0].answer,
-      Category: json[0].category
+      Category: json[0].category,
+      Points: json[0].points
     }),
     error => console.log(error)
   )
-}
-
-increasePoints(){
-  this.setState({
-    score: this.state.score + this.state.points
-  })
-}
-
-decreasePoints(){
-  this.setState({
-    score: this.state.score - this.state.points
-  })
-}
-
-toggleAnswer(){
-  this.setState({
-
-  })
-}
-
-revealQuestion(){
-  this.setState
 }
 
 render () {
@@ -61,24 +60,25 @@ render () {
     <h1>Welcome to Jeopardy!</h1>
     <h2>Score: {this.state.score}</h2>
     <div>
-      <button onclick={this.increasePoints}type='button'>Decrease</button>
-      <button onclick={this.decreasePoints}type='button'>Increase</button>
-      <button onclick={this.resetPoints}type='button'>Reset</button>
-      </div>
+    <button onclick={this.increasePoints}type='button'>Decrease</button>
+    <button onclick={this.decreasePoints}type='button'>Increase</button>
+    <button onclick={this.resetPoints}type='button'>Reset</button>
+    </div>
     <h2>Let's Play!</h2>
     <div>
-      <button onclick={this.revealQuestion}>Get Question</button>
-      <h2>Category: {this.state.category}</h2>
-      <h2>Points: {this.state.points}</h2>
-      <h2>Answer: {this.state.answer}</h2>
-      <button onclick={this.toggleAnswer}>Click to Reveal Question</button>
-      {(this.state.answer)
-      ? <toggleAnswer answer={this.state.answer}/>
+    <button onclick={this.revealQuestion}>Get Question</button>
+    <h2>Category: {this.state.category}</h2>
+    <h2>Points: {this.state.points}</h2>
+    <h2>Answer: {this.state.answer}</h2>
+    <button onclick={this.toggleAnswer}>Click to Reveal Question</button>
+    {(this.state.answer)
+      ? <h2><answer answer={this.state.answer}/></h2>
+      :null
     }
     </div>
     </body>
     </>
-    )
-  }
+  )
+}
 }
 export default App
